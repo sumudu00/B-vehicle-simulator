7882823ebab98a22ddb5255b3cbe2b2853cd618e
sumudu <sumudupriyankara263@gmail.com>
2024-07-12 16:08:05 +0530

    UI panel

---
+++ Script/UI Panel/rotater.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * 5, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * 5 ,0f));
+    }
+}
